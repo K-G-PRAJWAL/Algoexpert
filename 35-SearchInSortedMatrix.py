dc2707de5964023512a126e3b85d2bf87f8e234a
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2020-08-02 13:57:59 +0530

    Problem 35

---
+++ 35-SearchInSortedMatrix.py
@@ -0,0 +1,23 @@
+def searchInSortedMatrix(matrix, target):
+    '''
+    Time: O(N+M)
+    Space: O(1)
+    '''
+    row, col = 0, len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[row][col] < target:
+            row += 1
+        else:
+            return [row, col]
+    return [-1, -1]
+
+
+print(searchInSortedMatrix([
+    [1, 4, 7, 12, 15, 1000],
+    [2, 5, 19, 31, 32, 1001],
+    [3, 8, 24, 33, 35, 1002],
+    [40, 41, 42, 44, 45, 1003],
+    [99, 100, 103, 106, 128, 1004]
+], 99))
